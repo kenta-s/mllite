90771dfb790a6a85bea35a44bd8b93e13b74103b
kenta-s <knt01222@gmail.com>
2019-11-13 00:36:31 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -20,13 +20,13 @@ const IndexPage = () => {
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   // Tick the time every second
   const dispatch = useDispatch()
-  useInterval(() => {
-    dispatch({
-      type: 'TICK',
-      light: true,
-      lastUpdate: Date.now()
-    })
-  }, 1000)
+  // useInterval(() => {
+  //   dispatch({
+  //     type: 'TICK',
+  //     light: true,
+  //     lastUpdate: Date.now()
+  //   })
+  // }, 1000)
   return (
     <Layout>
       <Title>home</Title>
@@ -41,29 +41,29 @@ const IndexPage = () => {
   )
 }
 
-IndexPage.getInitialProps = ({ reduxStore }) => {
-  // fetchMlModels()
-  // axios.get('https://virtserver.swaggerhub.com/kenta-s/mllite/1.0.0/ml_models')
-  //   .then(response => {
-  //     console.log(response.data)
-  //     const dispatch = useDispatch()
-  //     dispatch({
-  //       type: 'INCREMENT'
-  //     })
-  //   })
-  //   .catch(error => {
-  //     console.error(error)
-  //   })
-  // Tick the time once, so we'll have a
-  // valid time before first render
-  const { dispatch } = reduxStore
-  dispatch({
-    type: 'TICK',
-    light: typeof window === 'object',
-    lastUpdate: Date.now()
-  })
-
-  return {}
-}
+// IndexPage.getInitialProps = ({ reduxStore }) => {
+//   // fetchMlModels()
+//   // axios.get('https://virtserver.swaggerhub.com/kenta-s/mllite/1.0.0/ml_models')
+//   //   .then(response => {
+//   //     console.log(response.data)
+//   //     const dispatch = useDispatch()
+//   //     dispatch({
+//   //       type: 'INCREMENT'
+//   //     })
+//   //   })
+//   //   .catch(error => {
+//   //     console.error(error)
+//   //   })
+//   // Tick the time once, so we'll have a
+//   // valid time before first render
+//   const { dispatch } = reduxStore
+//   dispatch({
+//     type: 'TICK',
+//     light: typeof window === 'object',
+//     lastUpdate: Date.now()
+//   })
+// 
+//   return {}
+// }
 
 export default withRedux(IndexPage)
