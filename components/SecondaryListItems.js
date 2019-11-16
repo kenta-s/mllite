aaa9474c42e093bb73398bf238cda27f11848b38
kenta-s <knt01222@gmail.com>
2019-11-16 19:10:07 +0900

    update

--- components/SecondaryListItems.js
+++ components/SecondaryListItems.js
@@ -6,13 +6,33 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { useTranslation } from "react-i18next";
+import Router from 'next/router'
+import { useDispatch } from 'react-redux'
+import { flashMessage } from 'redux-flash'
+import { withRedux } from '../lib/redux'
 
 const SecondaryListItems = () => {
   const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const removeCredentials = async () => {
+    localStorage.removeItem('expiry');
+    localStorage.removeItem('uid');
+    localStorage.removeItem('token-type');
+    localStorage.removeItem('access-token');
+    localStorage.removeItem('client');
+  }
+  const signOut = () => {
+    removeCredentials()
+      .then(() => {
+        Router.push('/sign_in')
+        dispatch(flashMessage(t('Signed out'), {isError: true}))
+      })
+  }
+
   return(
     <div>
       <ListSubheader inset>{t('user info')}</ListSubheader>
-      <ListItem button onClick={() => console.log('logout')}>
+      <ListItem button onClick={signOut}>
         <ListItemIcon>
           <ExitToAppIcon />
         </ListItemIcon>
@@ -22,4 +42,4 @@ const SecondaryListItems = () => {
   )
 }
 
-export default SecondaryListItems
+export default withRedux(SecondaryListItems)
