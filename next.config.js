9de0f5695ca8f3562248167031344501492e7e4e
kenta-s <knt01222@gmail.com>
2019-11-16 20:30:14 +0900

    update

--- next.config.js
+++ next.config.js
@@ -15,19 +15,3 @@ module.exports = {
     return config
   }
 }
-// module.exports = {
-//   webpack: (config, {}) => {
-//     config.module.rules.push({
-//       test: /.mdx/,
-//       use: [
-//         options.defaultLoaders.babel,
-//         {
-//           loader: '@mdx-js/loader',
-//           options: pluginOptions.options
-//         }
-//       ]
-//     })
-// 
-//     return config
-//   }
-// }
