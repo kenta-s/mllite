586c1eeec587f7e44dcab0a75bb270a8aee1f9e1
kenta-s <knt01222@gmail.com>
2019-11-17 15:55:11 +0900

    update

---
+++ pages/welcome.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from 'next/link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { useTranslation } from "react-i18next";
+import Layout from "../components/RegistrationLayout"
+import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { flashMessage } from 'redux-flash'
+import { withRedux } from '../lib/redux'
+import Router from 'next/router'
+
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const Welcome = () => {
+  const { t } = useTranslation()
+  const classes = useStyles();
+  const dispatch = useDispatch()
+
+  return (
+    <Layout>
+			<Avatar className={classes.avatar}>
+				<LockOutlinedIcon />
+			</Avatar>
+			<Typography component="h1" variant="h5">
+        {t('Registration completed')}
+			</Typography>
+      <p>
+        {t('Registration completed, build your model')}
+      </p>
+      <Link href="/sign_in">
+        <Button
+          variant="contained"
+          color="primary"
+        >
+          {t('Sign in')}
+        </Button>
+      </Link>
+    </Layout>
+  );
+}
+
+export default withRedux(Welcome)
